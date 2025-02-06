9e6833efb38883a855f0bc47d862e09441171783
viilasa <viilasacontact@gmail.com>
2025-02-06 16:49:33 +0530

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -231,7 +231,7 @@ const ProductCard = ({ product, category }: any) => {
             )} />
 
             <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex gap-2 z-10">
-              {product.images.map((_, index) => (
+              {product.images.map((_,:as unknown as any, index) => (
                 <button
                   key={index}
                   onClick={() => setCurrentImageIndex(index)}
