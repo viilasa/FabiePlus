0b3cef4773bd561a0cc27b925156fa6dde5c5563
viilasa <viilasacontact@gmail.com>
2025-02-18 14:27:16 +0530

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ import { TestimonialsSection } from '@/components/sections/testimonials';
 import { LocationsSection } from '@/components/sections/locations';
 import { ContactSection } from '@/components/sections/contact';
 import { FeaturedProducts } from '@/components/sections/featured-products';
+import { CustomersSection } from '@/components/sections/customers';
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <FeaturesSection />
+      <CustomersSection />
       <FeaturedProducts />
       <AboutSection />
       <TestimonialsSection />
